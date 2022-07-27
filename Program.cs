0109e70bde68909fd61cb7315f1644efc484d0c6
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-27 21:26:23 +0300

    Zadacha68

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два неотрицательных числа:");
+Console.Write("Введите число M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Akerman( int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m > 0) && (n == 0)) return Akerman(m - 1, 1);
+    else if ((m > 0) && (n > 0)) return Akerman(m - 1, Akerman(m, n - 1));
+    else return n + 1;
+}
+Console.WriteLine ($"Функция Аккермана равна {Akerman(m, n)}");
